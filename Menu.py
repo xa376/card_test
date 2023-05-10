2cc0713752ddb61c366a6630b9b68dd561ae1682
Xavier Hreben <projectfigirl@gmail.com>
2023-05-10 00:47:37 -0400

    Fixed exit.

--- Menu.py
+++ Menu.py
@@ -14,7 +14,7 @@ class Menu:
     def chooseOption(self, menuOptionNum):
 
 
-        if menuOptionNum > 0 and menuOptionNum < len(self.choices):
+        if menuOptionNum >= 0 and menuOptionNum < len(self.choices):
             self.choices[menuOptionNum][1]()
         else:
             print("Non-valid input")
